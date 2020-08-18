3bf6bd3bbf77b719d298790b81b742811a55830e
Abbas.d67@gmail.com <Abbas.d67@gmail.com>
2020-08-18 23:54:57 +0430

    Added Province Update/New/Delete Close #27 Added City update/New/Delete Close #26

--- MahtaKala/Controllers/Api/CitiesController.cs
+++ MahtaKala/Controllers/Api/CitiesController.cs
@@ -1,5 +1,6 @@
 ﻿using MahtaKala.ActionFilter;
 using MahtaKala.Entities;
+using MahtaKala.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -24,6 +25,41 @@ namespace MahtaKala.Controllers
         public async Task<List<City>> Index(long province_id)
         {
             return await db.Cities.Where(a => a.ProvinceId == province_id).ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index([FromBody] CityModel cityModel)
+        {
+            City city = null;
+            if (cityModel.Id == 0)
+            {
+                city = new City();
+                db.Cities.Add(city);
+            }
+            else
+            {
+                city = await db.Cities.FirstOrDefaultAsync(c => c.Id == cityModel.Id);
+                if (city == null)
+                    throw new Exception("City Not Found.");
+            }
+
+            city.Name = cityModel.City;
+            city.ProvinceId = cityModel.Province_Id;
+
+            await db.SaveChangesAsync();
+            return StatusCode(200);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Index([FromBody] IdModel model)
+        {
+            var city = await db.Cities.FirstOrDefaultAsync(c => c.Id == model.Id);
+            if (city == null)
+            {
+                throw new Exception("City Not Found.");
+            }
+            db.Cities.Remove(city);
+            await db.SaveChangesAsync();
+            return StatusCode(200);
         }
     }
 }
--- MahtaKala/Controllers/Api/ProvincesController.cs
+++ MahtaKala/Controllers/Api/ProvincesController.cs
@@ -1,5 +1,6 @@
 ﻿using MahtaKala.ActionFilter;
 using MahtaKala.Entities;
+using MahtaKala.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -24,6 +25,40 @@ namespace MahtaKala.Controllers.Api
         public async Task<List<Province>> Index()
         {
             return await db.Provinces.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index([FromBody] ProvinceModel provinceModel)
+        {
+            Province province = null;
+            if (provinceModel.Id == 0)
+            {
+                province = new Province();
+                db.Provinces.Add(province);
+            }
+            else
+            {
+                province = await db.Provinces.FirstOrDefaultAsync(c => c.Id == provinceModel.Id);
+                if (province == null)
+                    throw new Exception("Province Not Found.");
+            }
+
+            province.Name = provinceModel.Province;
+
+            await db.SaveChangesAsync();
+            return StatusCode(200);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Index([FromBody] IdModel model)
+        {
+            var province = await db.Provinces.FirstOrDefaultAsync(c => c.Id == model.Id);
+            if (province == null)
+            {
+                throw new Exception("Province Not Found.");
+            }
+            db.Provinces.Remove(province);
+            await db.SaveChangesAsync();
+            return StatusCode(200);
         }
     }
 }
