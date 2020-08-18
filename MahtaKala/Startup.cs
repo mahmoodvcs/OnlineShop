d94384e467ec9a07c18b60ebf88f8c0e728e26ca
mahmoodvcs <mahmood.dehghan@gmail.com>
2020-08-18 23:45:00 +0430

    Handle 404

--- MahtaKala/Startup.cs
+++ MahtaKala/Startup.cs
@@ -91,14 +91,7 @@ namespace MahtaKala
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
-                app.UseExceptionHandler("/error");
-            //}
+            app.UseExceptionHandler("/error");
 
             app.UseStaticFiles();
             if (!env.IsDevelopment())
