8ff7fc1fbd2dab7d94323c16b1727a60062e6fa7
mahmoodvcs <mahmood.dehghan@gmail.com>
2020-08-23 23:49:23 +0430

    Created Api to get all categories with some products

---
+++ MahtaKala/Models/ProductModels/CategoryWithProductsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MahtaKala.Models.ProductModels
+{
+    public class CategoryWithProductsModel
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+        public long? ParentId { get; set; }
+
+        public IList<CategoryWithProductsModel> Children { get; set; }
+        public IList<ProductConciseModel> Products { get; set; }
+    }
+}
