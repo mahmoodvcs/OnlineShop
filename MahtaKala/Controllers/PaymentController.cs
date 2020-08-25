ddbbeb7e42272268c7c67cee5c9b35e291ca8fd5
mahmoodvcs <mahmood.dehghan@gmail.com>
2020-08-25 17:15:25 +0430

    -

---
+++ MahtaKala/Controllers/PaymentController.cs
@@ -0,0 +1,19 @@
+﻿using MahtaKala.GeneralServices.Payment;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MahtaKala.Controllers
+{
+    public class PaymentController :Controller
+    {
+        public async Task<ActionResult> Pay()
+        {
+            ICKPaymentService paymentService = new ICKPaymentService();
+            var token = await paymentService.GetToken(1000);
+            return View();
+        }
+    }
+}
---
+++ MahtaKala/Infrustructure/AppSettings.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MahtaKala.Infrustructure
+{
+    public class AppSettings
+    {
+        public AppSettings(IConfiguration configuration, IWebHostEnvironment env)
+        {
+
+            StaticFilesPath = Path.Combine(env.WebRootPath, configuration.GetSection("AppSettings")["StaticFilesPath"]);
+            ProductImagesPath = Path.Combine(StaticFilesPath, "ProductImages");
+
+        }
+        public string JwtSecret { get; set; }
+        public string StaticFilesPath { get; private set; }
+        public string ProductImagesPath { get; private set; }
+    }
+}
