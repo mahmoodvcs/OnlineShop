140c89b61982165992ee4b0838805554f7da5319
mehdi <mehdijco@hotmail.com>
2020-09-23 18:01:07 +0330

    Fix Profike

--- MahtaKala/Helpers/Util.cs
+++ MahtaKala/Helpers/Util.cs
@@ -70,6 +70,63 @@ namespace MahtaKala.Helpers
             throw new Exception("کد ملی صحیح نیست");
         }
 
+
+        public static bool IsCheckNationalCode(string nationalCode ,out string msg)
+        {
+            //در صورتی که کد ملی وارد شده تهی باشد
+            msg = string.Empty;
+            if (String.IsNullOrEmpty(nationalCode))
+            {
+                msg = "لطفا کد ملی را صحیح وارد نمایید";
+                return false;
+            }
+               
+            //در صورتی که کد ملی وارد شده طولش کمتر از 10 رقم باشد
+            if (nationalCode.Length != 10)
+            {
+                msg = "طول کد ملی باید ده کاراکتر باشد";
+                return false;
+            }
+
+            //در صورتی که کد ملی ده رقم عددی نباشد
+            var regex = new Regex(@"\d{10}");
+            if (!regex.IsMatch(nationalCode))
+            {
+                msg = "کد ملی تشکیل شده از ده رقم عددی می‌باشد؛ لطفا کد ملی را صحیح وارد نمایید";
+                return false;
+            }
+
+            //در صورتی که رقم‌های کد ملی وارد شده یکسان باشد
+            var allDigitEqual = new[] { "0000000000", "1111111111", "2222222222", "3333333333", "4444444444", "5555555555", "6666666666", "7777777777", "8888888888", "9999999999" };
+            if (allDigitEqual.Contains(nationalCode))
+            {
+                msg = "کد ملی صحیح نیست";
+                return false;
+            }
+
+
+            //عملیات شرح داده شده در بالا
+            var chArray = nationalCode.ToCharArray();
+            var num0 = Convert.ToInt32(chArray[0].ToString()) * 10;
+            var num2 = Convert.ToInt32(chArray[1].ToString()) * 9;
+            var num3 = Convert.ToInt32(chArray[2].ToString()) * 8;
+            var num4 = Convert.ToInt32(chArray[3].ToString()) * 7;
+            var num5 = Convert.ToInt32(chArray[4].ToString()) * 6;
+            var num6 = Convert.ToInt32(chArray[5].ToString()) * 5;
+            var num7 = Convert.ToInt32(chArray[6].ToString()) * 4;
+            var num8 = Convert.ToInt32(chArray[7].ToString()) * 3;
+            var num9 = Convert.ToInt32(chArray[8].ToString()) * 2;
+            var a = Convert.ToInt32(chArray[9].ToString());
+
+            var b = (((((((num0 + num2) + num3) + num4) + num5) + num6) + num7) + num8) + num9;
+            var c = b % 11;
+
+            if (((c < 2) && (a == c)) || ((c >= 2) && ((11 - c) == a)))
+                return true;
+            msg = "کد ملی صحیح نیست";
+            return false;
+        }
+
         public static string Sub3Number(decimal price)
         {
             string value = price.ToString("0");
@@ -86,6 +143,12 @@ namespace MahtaKala.Helpers
         {
             Regex regex = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
             return regex.IsMatch(emailaddress);
+        }
+
+        public static bool IsAnyNumberInString(string text)
+        {
+            Regex regex = new Regex("[0-9]");
+            return regex.IsMatch(text);
         }
 
         public static bool IsNumber(string text)
