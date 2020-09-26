5e61909a9c929fb08944eb128c93ebd7b26c80cc
mahmoodvcs <mahmood.dehghan@gmail.com>
2020-09-26 17:49:36 +0330

    Enable CORS. Attempt 3

--- MahtaKala/Infrustructure/CorsMiddleware.cs
+++ MahtaKala/Infrustructure/CorsMiddleware.cs
@@ -16,13 +16,19 @@ namespace MahtaKala.Infrustructure
             _next = next;
         }
 
-        public Task Invoke(HttpContext httpContext)
+        public async Task Invoke(HttpContext httpContext)
         {
             httpContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
             httpContext.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
             httpContext.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version, X-File-Name");
             httpContext.Response.Headers.Add("Access-Control-Allow-Methods", "POST,GET,PUT,PATCH,DELETE,OPTIONS");
-            return _next(httpContext);
+            if(httpContext.Request.Method.ToUpper() == "OPTIONS")
+            {
+                httpContext.Response.StatusCode = 200;
+                return;
+            }
+            await _next(httpContext);
+            return;
         }
     }
 
--- MahtaKala/Startup.cs
+++ MahtaKala/Startup.cs
@@ -38,7 +38,7 @@ namespace MahtaKala
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddCors();
+            services.AddCors();
             //services.AddCors(options => options.AddPolicy(name: "AllowAll",
             //                  builder =>
             //                  {
