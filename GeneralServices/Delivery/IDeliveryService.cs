f56af399e9ae4b11337d51650a11bc4374f57206
mahmoodvcs <mahmood.dehghan@gmail.com>
2020-09-28 10:38:13 +0330

    YarBox 1

---
+++ GeneralServices/Delivery/IDeliveryService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MahtaKala.GeneralServices
+{
+    public interface IDeliveryService
+    {
+    }
+}
---
+++ GeneralServices/Delivery/YarBoxDeliveryService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MahtaKala.GeneralServices.Delivery
+{
+    public class YarBoxDeliveryService : IDeliveryService
+    {
+        const string APIAddress = "https://api.yarbox.co/api/v3/";
+    }
+
+
+    class CargoRequest
+    {
+        public CargoDestination destination { get; set; }
+        public CargoOrigin origin { get; set; }
+        public string receiveType { get; set; }
+        public bool isPacking { get; set; }
+        public int insurancePrice { get; set; }
+        public string content { get; set; }
+        public int postPackWeight { get; set; }
+        public int count { get; set; }
+    }
+
+    class CargoDestination
+    {
+        public string receiverPhoneNumber { get; set; }
+        public string receiverName { get; set; }
+        public int portId { get; set; }
+        public string province { get; set; }
+        public string city { get; set; }
+        public string street { get; set; }
+
+    }
+
+    class CargoOrigin
+    {
+        public string senderPhoneNumber { get; set; }
+        public string province => "تهران";
+        public string city => "تهران";
+        public string street { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+    }
+}
