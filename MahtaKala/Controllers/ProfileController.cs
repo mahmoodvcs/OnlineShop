a819a5fe39861c14eb2c982e9e7fa9eafe06d62d
Ali Dehghan <a.day23@gmail.com>
2020-10-19 08:06:36 +0330

    EditPhoneNumber: Almost done.

--- MahtaKala/Controllers/ProfileController.cs
+++ MahtaKala/Controllers/ProfileController.cs
@@ -8,6 +8,7 @@ using MahtaKala.GeneralServices;
 using MahtaKala.Helpers;
 using MahtaKala.Infrustructure.Exceptions;
 using MahtaKala.Models;
+using MahtaKala.Models.UserModels;
 using MahtaKala.Services;
 using MahtaKala.SharedServices;
 using Microsoft.AspNetCore.Mvc;
@@ -146,6 +147,80 @@ namespace MahtaKala.Controllers
 			//}
 		}
 
+		public IActionResult EditPhoneNumber(string number)
+		{
+			var user = db.Users.FirstOrDefault(x => x.Id == User.Id);
+			//if (user == null)
+			//{ }
+			var requestModel = new SignupRequest()
+			{ Mobile = user.MobileNumber };
+			if (!string.IsNullOrWhiteSpace(number))
+			{
+				requestModel.Mobile = number;
+			}
+			return View(requestModel);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> EditPhoneNumber(SignupRequest requestModel)
+		{
+			var number = Util.NormalizePhoneNumber(requestModel.Mobile);
+			if (!System.Text.RegularExpressions.Regex.IsMatch(number, @"^(\+98|0)?9\d{9}$"))
+			{
+				return Json(new { success = false, msg = "لطفا موبایل را به صورت صحیح وارد نمایید" });
+			}
+			var user = await db.Users.FirstOrDefaultAsync(x => x.Id == User.Id);
+			if (number.Equals(user.MobileNumber))
+			{
+				return Json(new { success = false, msg = "شماره موبایل تکراری!" });
+			}
+			
+			var code = await smsService.SendOTP(number, Messages.Messages.Signup.LoginOTPMessage);
+			UserActivationCode userCode = new UserActivationCode()
+			{
+				UserId = user.Id,
+				Code = code,
+				IssueTime = DateTime.Now,
+				ExpireTime = DateTime.Now.AddMinutes(2),
+				AdditionalData = number
+			};
+			db.UserActivationCodes.Add(userCode);
+			await db.SaveChangesAsync();
+
+			return Json(new { success = true, msg = "ارسال با موفقیت انجام شد", id = userCode.Id });
+		}
+
+		public IActionResult ConfirmPhoneNumber(int id)
+		{
+			var confirmRequestModel = new VerifyRequest();
+			confirmRequestModel.Id = id;
+			return PartialView(confirmRequestModel);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> ConfirmPhoneNumber(VerifyRequest verifyRequest)
+		{
+			var userCode = db.UserActivationCodes.FirstOrDefault(a => a.Id == verifyRequest.Id);
+			var user = await db.Users.FirstOrDefaultAsync(a => a.Id == userCode.UserId);
+			if (user == null)
+			{
+				return Json(new { success = false, msg = "درخواست نا معتبر می باشد" });
+			}
+			if (userCode.Code != verifyRequest.Code)
+			{
+				return Json(new { success = false, msg = "کد ارسالی نامعتبر می باشد" });
+			}
+			if (userCode.ExpireTime < DateTime.Now)
+			{
+				return Json(new { success = false, msg = "زمان ثبت درخواست به اتمام رسیده است" });
+			}
+
+			user.MobileNumber = userCode.AdditionalData;
+			db.Entry(user).State = EntityState.Modified;
+			await db.SaveChangesAsync();
+
+			return Json(new { success = true });
+		}
 
 		public async Task<IActionResult> AddressEdit(long Id)
 		{
---
+++ MahtaKala/wwwroot/js/EditPhoneNumber.js
@@ -0,0 +1,86 @@
+﻿
+$(document).on("submit", "form#EditPhoneNumber", function (e) {
+    e.preventDefault();
+    $.blockUI({
+        message: '<img src="/img/loading.gif"/>',
+        css: {
+            border: 'none',
+            backgroundColor: 'transparent'
+        }
+    });
+    var action = $(this).attr('action');
+    var formdata = new FormData($('#EditPhoneNumber').get(0));
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: action,
+        cache: false,
+        data: formdata,
+        processData: false,
+        contentType: false,
+        beforeSend: function () {
+        },
+        success: function (res) {
+            $.unblockUI();
+            if (res.success) {
+                $("#uxDivContent").load("/Profile/ConfirmPhoneNumber/" + res.id);
+            }
+            else {
+                toastr.warning(res.msg, '', { positionClass: "toast-bottom-center" });
+            }
+
+        },
+        error: function (xhr) {
+            $.unblockUI();
+            swal("خطایی در ثبت اطلاعات اتفاق افتاده است", "", "warning");
+        },
+        complete: function () {
+            $.unblockUI();
+        }
+    });
+    return false;
+});
+
+
+
+$(document).on("submit", "form#ConfirmPhoneNumber", function (e) {
+    e.preventDefault();
+    $.blockUI({
+        message: '<img src="/img/loading.gif"/>',
+        css: {
+            border: 'none',
+            backgroundColor: 'transparent'
+        }
+    });
+    var action = $(this).attr('action');
+    var formdata = new FormData($('#ConfirmPhoneNumber').get(0));
+    $.ajax({
+        type: "POST",
+        dataType: "json",
+        url: action,
+        cache: false,
+        data: formdata,
+        processData: false,
+        contentType: false,
+        beforeSend: function () {
+        },
+        success: function (res) {
+            $.unblockUI();
+            if (res.success) {
+                window.location.href = "/home/index";
+            }
+            else {
+                toastr.warning(res.msg, '', { positionClass: "toast-bottom-center" });
+            }
+
+        },
+        error: function (xhr) {
+            $.unblockUI();
+            swal("خطایی در ثبت اطلاعات اتفاق افتاده است", "", "warning");
+        },
+        complete: function () {
+            $.unblockUI();
+        }
+    });
+    return false;
+});
