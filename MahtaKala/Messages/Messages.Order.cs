c1a49668a30a347d8ca6ff5b5641974b9f70c6cd
Ali.D <a.day23@gmail.com>
2020-10-19 19:25:08 +0330

    -Modify the text (sms) sent to the user after a successful purchase (issue #203)

--- MahtaKala/Messages/Messages.Order.cs
+++ MahtaKala/Messages/Messages.Order.cs
@@ -16,7 +16,7 @@ namespace MahtaKala.Messages
             public const string ErrorWrongTrackNo = "شماره پیگیری وارد شده با شماره پیگیری همخوانی ندارد.";
             public const string DeliveredOTPMessage = "مهتا کالا: کالای شما ارسال شد. کد دریافت کالا: {0}. در هنگام تحویل، این کد را به پیک ارائه دهید";
             public const string ProductDoesNotExistInStore ="کالای {0} در انبار موجود نیست";
-            public const string OrderPaymentSuccessMessage = "مهتاکالا: پرداخت انجام شد. کد رهگیری: {0}. تاریخ تخمینی ارسال کالا: {1}";
+            public const string OrderPaymentSuccessMessage = "مهتاکالا: پرداخت انجام شد. کد تحویل: {0}. ارائه کد فوق هنگام تحویل کالای خریداری شده الزامی است. تاریخ تخمینی ارسال کالا: {1}";
          
             public const string CannotAddProduct_DefferentSeller = "امکان افزودن این کالا وجود ندارد.";//فروشنده ی این کالا، با کالاهای قبلی متفاوت است
             public const string CannotAddProduct_NotAvailable = "کالای {0} موجود نیست.";
