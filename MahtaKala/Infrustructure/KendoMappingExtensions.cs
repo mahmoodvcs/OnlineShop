bb028a113a0b36f3bb24b346b269a00a0c6458e6
mahmoodvcs <mahmood.dehghan@gmail.com>
2020-10-24 07:09:05 +0330

    Display product image in list

--- MahtaKala/Infrustructure/KendoMappingExtensions.cs
+++ MahtaKala/Infrustructure/KendoMappingExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Kendo.Mvc.Extensions;
 using Kendo.Mvc.UI;
 using MahtaKala.Entities.Models;
+using MahtaKala.Services;
 using MahtaKala.SharedServices;
 using System;
 using System.Collections.Generic;
@@ -13,11 +14,12 @@ namespace MahtaKala.Infrustructure
     {
         public static async Task<DataSourceResult> ToListResultAsync(this IQueryable<ProductModel> query, DataSourceRequest request)
         {
+            var productImageService = MyServiceProvider.ResolveService<IProductImageService>();
             return await query.ToDataSourceResultAsync(request, a => new ProductListModel
             {
                 Id = a.Id,
                 Title = a.Title,
-                Thubmnail = a.Thubmnail,
+                Thubmnail = productImageService.GetImageUrl(a.Id, a.Thubmnail),
                 Categories = string.Join(",", a.Categories),
                 Tags = string.Join(",", a.Tags),
                 Brand = a.Brand,
