704b8976d80acc8f5699219b1b576a284b1811d0
mahmoodvcs <mahmood.dehghan@gmail.com>
2020-10-24 04:40:04 +0330

    Export products

--- Entitties/Models/ProductListModel.cs
+++ Entitties/Models/ProductListModel.cs
@@ -1,23 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace MahtaKala.Entities.Models
 {
     public class ProductListModel
     {
+        [Display(Name ="شناسه")]
         public long Id { get; set; }
-
+        [Display(Name = "نام")]
         public string Title { get; set; }
+        [Display(Name ="عکس")]
         public string Thubmnail { get; set; }
+        [Display(Name = "دسته بندی")]
         public string Categories { get; set; }
+        [Display(Name = "تگ ها")]
         public string Tags { get; set; }
+        [Display(Name = "برند")]
         public string Brand { get; set; }
+        [Display(Name = "قیمت")]
         public decimal? Price { get; set; }
+        [Display(Name = "قیمت با تخفیف")]
         public decimal? DiscountPrice { get; set; }
+        [Display(Name = "وضعیت")]
         public string Status { get; set; }
+        [Display(Name = "فروشنده")]
         public string Seller { get; set; }
+        [Display(Name = "کد محصول")]
         public string Code { get; set; }
+        [Display(Name = "منتشر شده")]
         public bool Published { get; set; }
     }
 }
