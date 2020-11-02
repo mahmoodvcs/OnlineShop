35dcaad336b9452fdb53fcfb81e3416fd7d9b437
Ali.D <a.day23@gmail.com>
2020-11-02 12:43:38 +0330

    -Bug Fix: Issue #238
    Bug in finding empty rows [Solved]

--- GeneralServices/Messages/ServiceMessages.Import.cs
+++ GeneralServices/Messages/ServiceMessages.Import.cs
@@ -9,8 +9,8 @@ namespace MahtaKala.GeneralServices
         public static class Import
         {
             public const string ImportingRow = "دریافت اطلاعات سطر {0}. ";
-            public const string ProductCannotBeFound = "محصول '{0}' پیدا نشد";
-            public const string DuplicateProduct = "محصول '{0}' تکراری است";
+            public const string ProductCannotBeFound = "محصول سطر {0} در فایل اکسل پیدا نشد";
+            public const string DuplicateProduct = "محصول {0} تکراری است";
             public const string PriceIsEmpty = "قیمت مشخص نشده است.";
 
         }
