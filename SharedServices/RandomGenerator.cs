34d4bba4c451d64db9e512b659d38cab320f5b3c
Ali.D <a.day23@gmail.com>
2020-12-09 19:22:13 +0330

    untracked files on master: 9e69c70 -Debug & Feature: Share Payment + OrphanOrderCatcher + Staff/ShareOrderPayment

---
+++ SharedServices/RandomGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MahtaKala.SharedServices
+{
+	public class RandomGenerator : Random
+	{
+		public RandomGenerator(int seed) : base(seed)
+		{ }
+		
+		public RandomGenerator() : this(DateTime.Now.Ticks)
+		{ }
+
+		public RandomGenerator(long seed) : base((int)(seed % int.MaxValue))
+		{ }
+	}
+}
