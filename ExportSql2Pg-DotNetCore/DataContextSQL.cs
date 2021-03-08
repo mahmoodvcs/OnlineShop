681fd951ca6689cf965fe599e2d7bf897836d4f1
Ali.D <a.day23@gmail.com>
2021-03-08 18:13:00 +0330

    -I'm crossing my fingers like the head-nun of the church of St. Jerome, accidentally participating in a nation-wide orgy, crossing her legs to scare-off perverts, and also, erotic thoughts! That's how hard I'm crossing my fingers!
    Will it work?

--- ExportSql2Pg-DotNetCore/DataContextSQL.cs
+++ ExportSql2Pg-DotNetCore/DataContextSQL.cs
@@ -2,12 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
 
 namespace ExportSql2Pg_DotNetCore
 {
     internal class DataContextSQL : MahtaContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Server=Mahta.Eskad.ir;Database=Mahta;uid=sa;pwd=@Mahta@&1399!");
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        { 
+            optionsBuilder.UseSqlServer("Server=Mahta.Eskad.ir;Database=Mahta;uid=sa;pwd=@Mahta@&1399!");
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+        }
     }
 }
